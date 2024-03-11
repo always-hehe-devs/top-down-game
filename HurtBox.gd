8e1d431f4747a810d45eda46dd16346d8714ee04
Pawel Fajfer <fajfer.pawel@gmail.com>
2024-03-11 08:55:20 +0100

    Added random mob spawn

--- HurtBox.gd
+++ HurtBox.gd
@@ -8,6 +8,7 @@ func _ready() -> void:
 	connect("area_entered", _on_area_entered)
 
 func _on_area_entered(hitbox: HitBox):
+	print(hitbox)
 	if hitbox.owner.get_parent() != owner && owner.has_method("take_damage"):
 		owner.take_damage(hitbox.damage)
 		hitbox.on_hit()
