16001e6ee1a5038087479519c2d2c27e8c96f383
Pawel Fajfer <fajfer.pawel@gmail.com>
2024-03-26 19:53:22 +0100

    Added new assets to tileset

--- TileMapForeground.gd
+++ TileMapForeground.gd
@@ -4,8 +4,11 @@ var texture = load("res://2d_lights_and_shadows_neutral_point_light.webp")
 
 func _ready():
 	for cell in get_used_cells(0):
-		var point_light := PointLight2D.new()
-		point_light.texture = texture
-		point_light.scale = Vector2(0.45,0.45)
-		point_light.position = map_to_local(cell)
-		add_child(point_light)
+		var cell_data = get_cell_tile_data(0, cell)
+		if cell_data.get_custom_data("light_source"):
+			var point_light := PointLight2D.new()
+			point_light.texture = texture
+			point_light.scale = Vector2(0.45,0.45)
+			point_light.position = map_to_local(cell)
+			point_light.color.a = 0.5
+			add_child(point_light)
